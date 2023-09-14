04060709a5d339ea7c07374b885f7bf43e30ac23
Amos juda <amosjudasilva@gmail.com>
2023-09-14 08:19:35 -0300

    Update past commit

--- SistemaProgII/src/br/edu/unicesumar/core/dao/ProdutoDAO.java
+++ SistemaProgII/src/br/edu/unicesumar/core/dao/ProdutoDAO.java
@@ -4,6 +4,8 @@ import br.edu.unicesumar.core.dao.conexao.ConexaoJDBC;
 import br.edu.unicesumar.core.entity.Produto;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+
 
 public class ProdutoDAO {
     public void inserir(Produto produto){            
@@ -22,5 +24,30 @@ public class ProdutoDAO {
         } catch(SQLException e){
             e.printStackTrace();
         }
+    }
+    public Produto buscarProdutoPorId(Long id){
+        String sql = "SELECT ID, NOME, VALOR_CUSTO, VALOR_VENDA, QUANTIDADE WHERE ID = ?";
+        
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        try{
+            ps = ConexaoJDBC.getConexao().prepareStatement(sql);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
+            
+            Produto produto = new Produto();
+            if(rs.next()){
+                produto.setId(rs.getLong("ID"));
+                produto.setNome(rs.getString("NOME"));
+                produto.setValorCusto(rs.getDouble("VALOR_CUSTO"));
+                produto.setValorVenda(rs.getDouble("VALOR_VENDA"));
+                produto.setQtdEstoque(rs.getInt("QUANTIDADE"));
+            }
+            return produto;
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
